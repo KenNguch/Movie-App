56ba8c3f7a4e9165a6cd2574d8192700496d391e
kennguch <kennguch3@gmail.com>
2021-04-20 11:32:33 +0300

    credentials classs added

---
+++ app/src/main/java/com/kennguch/movie/utils/Credentials.java
@@ -0,0 +1,9 @@
+package com.kennguch.movie.utils;
+
+public class Credentials {
+
+    public static final String BASE_URL = "https://api.themoviedb.org/";
+
+    public static final String API_KEY = "fbf9310c4e3877142bc02ac27843d305";
+
+}
